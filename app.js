a43c03905e8bf46c0056ae6b4664e7bcb11accb2
aary0605 <patel6aarya@gmail.com>
2026-08-25 23:00:02 +0530

    Add app

---
+++ app.js
@@ -0,0 +1,36 @@
+// Express js framework
+
+// Introduction to Express js 
+// it is an npm package   
+// framework => it is a flow. 
+// manages everything from receiving the request and givng the response
+
+// const express = require('express');
+
+// const app = express();
+
+// // app.use(function(req,res,next) {
+// //   console.log('Middleware executed');
+// //   next();
+// // });
+
+// app.post('/',(req,res) => {
+//   console.log('req');
+//   res.send('Hello man');
+// })
+// app.get('/',(req,res)=> {
+//   res.send('Hello world')
+// })
+// // app.use((err,req,res,next) => {
+// //   res.status(500)
+// // })
+
+// app.listen(3000,()=> {
+//   console.log('Server Started')
+// });
+
+// const path = require('path');
+
+// console.log(process.getuid);
+
+
