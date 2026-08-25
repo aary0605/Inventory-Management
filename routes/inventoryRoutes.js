5f6fab5b740415c3b8082a0a759f853f574736c6
aary0605 <patel6aarya@gmail.com>
2026-08-26 00:49:09 +0530

    Add new Route

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,7 +1,7 @@
 const app = require('express');
 
 const router = app.Router();
-const {dataHandling,productsHistory,filterByCategory,filterByPrice,deleteProducts} = require('../contollers/inventoryControllers')
+const {dataHandling,productsHistory,filterByCategory,filterByPrice,deleteProducts,productId} = require('../contollers/inventoryControllers')
 
 // router.patch('/inventory/edit',)
 
@@ -9,7 +9,7 @@ router.get('/inventory/history',productsHistory)
 router.get('/inventory/category',filterByCategory);
 router.get('/inventory/price',filterByPrice);
 router.delete('/inventory/delete',deleteProducts);
-router.get('/inventory/product/data')
+router.get('/inventory/product/data',productId)
 
 router.post('/inventory/product',dataHandling);
 
