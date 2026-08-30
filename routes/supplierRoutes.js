0b6374a50aa78f7aef41405801859e1f2b90bd88
aary0605 <patel6aarya@gmail.com>
2026-08-30 18:54:57 +0530

    Add route to get supplier data

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,11 +1,12 @@
 const app = require('express');
 const router = app.Router();
 
-const {supplier,supplierHistory,editSupplier, supplierDelete} = require('../contollers/supplierController');
+const {supplier,supplierHistory,editSupplier, supplierDelete,fetchSupplier} = require('../contollers/supplierController');
 
 
 router.get('/supplier/history', supplierHistory);
 router.delete('/supplier/delete',supplierDelete);
+router.get('/supplier/edit/data',fetchSupplier)
 router.patch('supplier/data',editSupplier);
 
 router.post('/supplier/add',supplier);
