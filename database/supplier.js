36e2103724253bd9bd327f66d4c150169e7b1bd8
aary0605 <patel6aarya@gmail.com>
2026-08-30 18:20:21 +0530

    Changes in db function

--- database/supplier.js
+++ database/supplier.js
@@ -1,13 +1,9 @@
 const pool = require('../config/db');
 
 async function supplierData() { 
-  const res =await  pool.query(`
-    select supplier.*,count(products.product) as total_products from supplier 
-    left join products on supplier.supplier_id = products.supplier_id 
-    group by supplier.supplie_name,supplier.supplier_id;`);
-    
-    return res[0];
-    
+ const query = await pool.query(`
+  select * from supplier;`);
+  return query[0];
 }
 
 
@@ -15,11 +11,11 @@ async function addSupplier(...data){
   try {
 
     const res = pool.query(`
-      insert into supplier(supplier_id,supplie_name,
+      insert into supplier(supplie_name,
     supplier_city,
     supplier_contact,
     supplier_email) 
-    values(?,?,?,?,?);`,[data[0].id,data[0].supplier,data[0].city,data[0].phone,data[0].email]);
+    values(?,?,?,?,?);`,[data[0].supplier,data[0].city,data[0].phone,data[0].email]);
    console.log("Data Added"); 
   }
   catch(err) {
