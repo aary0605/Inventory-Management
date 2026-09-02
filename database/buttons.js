d5a8826cd264d462b939c2b8f4f7c546a1de0f36
aary0605 <patel6aarya@gmail.com>
2026-09-02 16:13:41 +0530

    Add Edit Products query

--- database/buttons.js
+++ database/buttons.js
@@ -20,9 +20,20 @@ async function deleteProduct(id) {
     const setKey = await pool.query(`
       SET FOREIGN_KEY_CHECKS = 1;`);
   }
-    
-  
-    
     // console.log("Item Deleted")
 }
-module.exports = deleteProduct;
+
+async function editProducts(data) {
+  try {
+
+    const query = await pool.query(`
+      update products 
+      set product = '${data.product}',qty= ${data.qty},category = '${data.category}',price =${data.price},supplier = '${data.supplier}' 
+      where id = ${data.id};`)
+      console.log('Data Updated');
+   } 
+   catch(err){
+    console.log(err)
+   }
+  }
+module.exports = {deleteProduct,editProducts};
